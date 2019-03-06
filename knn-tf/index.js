47e25f7e3911e2a7920cd389c0bddb1751aedda8
Abe <absolutus11@gmail.com>
2019-03-07 03:02:05 +0400

    standardization applied

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,32 +2,40 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+// load data, shuffle, split and extract features and labels for test and training
 let { features, labels, testFeatures, testLabels } = loadCSV(
   'kc_house_data.csv',
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ['lat', 'long'],
+    dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
     labelColumns: ['price']
   }
 );
 
 function knn(features, labels, point, k) {
+  const { mean, variance } = tf.moments(features);
+
+  // standardizing the test sample
+  const scaledPoint = point.sub(mean).div(variance.pow(0.5));
+
   return (
-    // finding the difference between feature and point
+    // find the difference between feature and point
     features
-      .sub(point)
+      .sub(mean)
+      .div(variance.pow(0.5)) // standardizing the features
+      .sub(scaledPoint)
       .pow(2)
       .sum(1)
-      .pow(0.5) // getting the distance between two points
+      .pow(0.5) // get the distance between two points
       .expandDims(1)
-      .concat(labels, 1) // joining features and labels tensor arrays
-      .unstack() // converting to an array of tensor
-      // sorting the array of tensors by distance (lowest first)
+      .concat(labels, 1) // join features and labels tensor arrays
+      .unstack() // convert to an array of tensor
+      // sort the array of tensors by distance (lowest first)
       .sort((a, b) => {
         return a.get(0) > b.get(0) ? 1 : -1;
       })
-      // extracting closest k tensors, summing the labels and finding the average
+      // extract closest k tensors, sum the labels and find the average
       .slice(0, k)
       .reduce((sum, tensor) => {
         return sum + tensor.get(1);
@@ -35,10 +43,13 @@ function knn(features, labels, point, k) {
   );
 }
 
+// convert to tensors
 features = tf.tensor(features);
 labels = tf.tensor(labels);
-testFeatures = tf.tensor(testFeatures[0]);
 
-const result = knn(features, labels, testFeatures, 10);
-
-console.log('guess', result, testLabels[0][0]);
+// run error percentage analysis for all test samples
+testFeatures.forEach((testPoint, i) => {
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const err = (testLabels[i][0] - result) / testLabels[i][0];
+  console.log('error', err * 100);
+});
