5a5c34ad67bdae842a29e9ce68f41e4b1e0f7930
Abe <absolutus11@gmail.com>
2019-03-07 08:35:15 +0400

    data splitting, shuffling and extracting added

--- loadcsv/load-csv.js
+++ loadcsv/load-csv.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const _ = require('lodash');
 const shuffleSeed = require('shuffle-seed');
 
+//  extracts only the necessary data using the indexes of column names
 function extractColumns(data, columnNames) {
   const headers = _.first(data);
 
@@ -22,35 +23,38 @@ function loadCSV(
     dataColumns = [],
     labelColumns = [],
     shuffle = true,
-    shuffleString = 'defaultString',
-    converters = {}
+    splitTest = false, // boolean or integer ( if true, the data is divided equally)
+    shuffleString = 'defaultString', // change the string to reshuffle
+    converters = {} // custom converter function for the column values
   }
 ) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
   let labels = [];
 
-  //   get array of arrays
+  // get array of arrays
   data = data.split('\n').map((row) => {
     return row.split(',');
   });
 
-  //   removing trailing commas
+  // removing trailing commas
   data = data.map((row) => {
     return _.dropRightWhile(row, (value) => {
       return value === '';
     });
   });
 
-  //   get the headers row
+  // get the headers row
   const headers = _.first(data);
 
+  // parse data using a custom converter or turn into numbers
   data = data.map((row, index) => {
     if (index === 0) {
       return row;
     }
     return row.map((el, index) => {
       if (converters[headers[index]]) {
-        const converted = converters[headers[index]](el);
+        // check for a method named like a column
+        const converted = converters[headers[index]](el); // call it with an element as arg
         return _.isNaN(converted) ? el : converted;
       }
       const result = parseFloat(el);
@@ -58,27 +62,50 @@ function loadCSV(
     });
   });
 
-  labels = extractColumns(data, labelColumns);
+  // extracting features and labels
   data = extractColumns(data, dataColumns);
+  labels = extractColumns(data, labelColumns);
 
-  labels.shift();
   data.shift();
+  labels.shift();
 
+  // the same shuffle string is used to get matching results
   if (shuffle) {
-    labels = shuffleSeed.shuffle(labels, shuffleString);
     data = shuffleSeed.shuffle(data, shuffleString);
+    labels = shuffleSeed.shuffle(labels, shuffleString);
   }
 
-  console.log(labels);
-  console.log(data);
+  // split the features and labels in half or using the splitTest number
+  if (splitTest) {
+    const testSize = _.isNumber(splitTest)
+      ? splitTest
+      : Math.floor(data.length * 0.5);
+
+    return {
+      features: data.slice(testSize),
+      labels: labels.slice(testSize),
+      testFeatures: data.slice(0, testSize),
+      testLabels: labels.slice(0, testSize)
+    };
+  } else {
+    return { features: data, labels }; // return data without splitting
+  }
 }
 
-loadCSV('./data.csv', {
-  dataColumns: ['id', 'height'],
-  labelColumns: ['passed'],
-  converters: {
-    passed: (value) => {
-      return value === 'TRUE' ? 1 : 0;
-    }
-  }
-});
+// test call
+
+// const { features, labels, testFeatures, testLabels } = loadCSV('./data.csv', {
+//   dataColumns: ['id', 'height'],
+//   labelColumns: ['passed'],
+//   splitTest: 1,
+//   converters: {
+//     passed: (value) => {
+//       return value === 'TRUE' ? 1 : 0;
+//     }
+//   }
+// });
+
+// console.log('features', features);
+// console.log('labels', labels);
+// console.log('testFeatures', testFeatures);
+// console.log('testLabels', testLabels);
